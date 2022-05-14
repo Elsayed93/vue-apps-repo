4cb913e7d3118533b78364d0310d297aec704c28
Elsayed Elbeshry <elsayedelbeshry119@gmail.com>
2022-05-14 21:41:14 +0200

    Header Component and Logout

---
+++ restraunt-app/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home-nav">
+    <a href="#">Home</a>
+    <a>Add Restaurant</a>
+    <a>Update</a>
+    <a href="#" @click.prevent="logout">Logout</a>
+  </div>
+  <h1>Header</h1>
+</template>
+
+<script>
+export default {
+  name: "Header",
+
+  methods: {
+    logout() {
+      localStorage.removeItem("user_info");
+      this.$router.push({ name: "Login" });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- restraunt-app/src/views/HomeView.vue
+++ restraunt-app/src/views/HomeView.vue
@@ -1,23 +1,29 @@
 <template>
   <main>
     <div class="home-title">
-      <h1>Home PAge , Welcome ...</h1>
+      <Header />
+
+      <div class="content">
+        <h1>Home PAge , Welcome ...</h1>
+      </div>
     </div>
   </main>
 </template>
 
 <script>
 import SignUp from "../components/SignUp.vue";
+import Header from "../components/Header.vue";
 
 export default {
   components: {
     SignUp,
+    Header,
   },
   mounted() {
     let user = localStorage.getItem("user_info");
 
     if (!user) {
-      this.$router.push({ name: "signUp" });
+      this.$router.push({ name: "Login" });
     }
   },
 };
@@ -26,8 +32,34 @@ export default {
 
 <style>
 .home-title {
-  margin: 10rem auto 0;
+  margin: 0rem auto;
   width: fit-content;
   text-align: center;
+}
+
+.content {
+  margin-top: 10rem;
+}
+
+/* for header component */
+.home-nav {
+  display: flex;
+  justify-content: space-between;
+  background-color: #4e4e4e;
+  color: white;
+  border-radius: 0.25rem;
+  margin-bottom: 2rem;
+}
+
+.home-nav a {
+  padding: 1rem;
+  color: white;
+  text-decoration: none;
+}
+
+.home-nav a:hover {
+  color: white !important;
+  background-color: grey;
+  text-decoration: none;
 }
 </style>
